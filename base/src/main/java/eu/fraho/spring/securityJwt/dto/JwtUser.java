528e8c7556288a66f42fb770c75bea3f852951c9
Simon Frankenberger <simon@fraho.eu>
2017-06-06 20:05:10 +0200

    Make JwtUser.authority deprecated

--- base/src/main/java/eu/fraho/spring/securityJwt/dto/JwtUser.java
+++ base/src/main/java/eu/fraho/spring/securityJwt/dto/JwtUser.java
@@ -38,17 +38,26 @@ public class JwtUser implements UserDetails, CredentialsContainer {
     private boolean credentialsNonExpired = false;
     private boolean enabled = false;
     @JsonIgnore
-    private List<GrantedAuthority> authorities = Collections.emptyList();
+    private List<GrantedAuthority> authorities = new ArrayList<>();
     @Deprecated
     private String authority = "NONE";
     private boolean apiAccessAllowed = false;
+
+    @Deprecated
+    public String getAuthority() {
+        return authorities.isEmpty() ? null : authorities.get(0).toString();
+    }
+
+    @Deprecated
+    public void setAuthority(String authority) {
+        this.authorities.add(new SimpleGrantedAuthority(authority));
+    }
 
     public static JwtUser fromClaims(JWTClaimsSet claims) {
         JwtUser user = new JwtUser();
         user.setUsername(claims.getSubject());
         final Object oldAuthority = claims.getClaim("authority");
         if (oldAuthority != null && oldAuthority instanceof String) {
-            user.setAuthority(String.valueOf(oldAuthority));
             user.setAuthorities(Collections.singletonList(new SimpleGrantedAuthority(String.valueOf(oldAuthority))));
         } else {
             final List<String> claimAuthorities = (List<String>) claims.getClaim("authorities");
@@ -57,7 +66,6 @@ public class JwtUser implements UserDetails, CredentialsContainer {
                 newAuthorities.add(new SimpleGrantedAuthority(authority));
             }
             user.setAuthorities(newAuthorities);
-            user.setAuthority(newAuthorities.get(0).toString());
         }
         user.setId(Long.valueOf(String.valueOf(claims.getClaim("uid"))));
         return user;
